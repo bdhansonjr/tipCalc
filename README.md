822f837d802d783ef77356776ed3ec5985e7e6c8
RobinsonKameron <ka12@umbc.edu>
2015-12-25 20:26:52 -0500

    updated settings view

---
+++ README.md
@@ -0,0 +1,20 @@
+# tipCalc
+*codePath prework*
+
+This repository is my preworfk for the codepath iOS course at UMBC
+
+#*Install and run this app*
+
+To run this application you need to have Xcode installed to your mac + a simulator.
+ - The supported devices for this application are iPhone 6 and 6s currently
+
+
+#*Make this app*
+If you're interested in learning iOS development with swift check out the link below
+
+  - http://courses.codepath.com/snippets/intro_to_ios/prework.md
+  
+  
+  
+#Preview
+![alt tag](https://github.com/RobinsonKameron/tipCalc/blob/master/gifs/tipCalc_Phase2.gif?raw=true)
---
+++ tipCalc/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  tipCalc
+//
+//  Created by Robinson Kameron on 12/23/15.
+//  Copyright © 2015 Robinson Kameron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+ 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var settingsButton: UIBarButtonItem!
+    @IBOutlet weak var defPercentage: UISegmentedControl!
+    
+    @IBAction func actDefault(sender: UISegmentedControl) {
+        //Code for selecting defaults
+        print(defPercentage.selectedSegmentIndex)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        print("User edited bill")
+        
+        let tipPercentages = [0.18, 0.20, 0.22]
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        // Conver the str billField to floating point val
+        let billFieldVal = (billField.text! as NSString).doubleValue
+        
+        // check value of billFieldVal
+        print(billFieldVal)
+        let tip = billFieldVal * tipPercentage
+        let total = tip + billFieldVal
+
+        //Update lables in view
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        // Format to two decimal places
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+
+}
+
